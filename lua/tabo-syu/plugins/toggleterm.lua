88e6d2a1ec4ea2f0c138b12931e450f66c2e1cb6
tabo-syu <takeshima.nitkc@gmail.com>
2024-09-08 16:39:43 +0900

    add toggleterm config

--- lua/tabo-syu/plugins/toggleterm.lua
+++ lua/tabo-syu/plugins/toggleterm.lua
@@ -1,3 +1,12 @@
 return {
-  { "akinsho/toggleterm.nvim", version = "*", config = true }
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require('toggleterm').setup({
+      open_mapping = [[<C-b>]],
+      direction = "horizontal",
+      start_in_insert = true,
+    })
+  end
 }
+
